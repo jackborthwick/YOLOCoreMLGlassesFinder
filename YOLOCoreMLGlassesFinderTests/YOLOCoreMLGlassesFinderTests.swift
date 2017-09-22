8a6e938a0f42d859e8eb92456a98765615fea58b
Jack Borthwick <jackborthwick@Jacks-MacBook-Pro.local>
2017-09-22 12:34:55 -0600

    Initial Commit

---
+++ YOLOCoreMLGlassesFinderTests/YOLOCoreMLGlassesFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  YOLOCoreMLGlassesFinderTests.swift
+//  YOLOCoreMLGlassesFinderTests
+//
+//  Created by Jack Borthwick on 9/22/17.
+//  Copyright © 2017 Jack Borthwick. All rights reserved.
+//
+
+import XCTest
+@testable import YOLOCoreMLGlassesFinder
+
+class YOLOCoreMLGlassesFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
